2c17673fe11858d18497f1d5af261ce0f1bf0e7c
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2023-12-30 12:51:44 -0800

    project trim

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,59 +9,53 @@ import HomePage from './pages/Home';
 import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
-import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <HomePage /> },
-      {
-        path: 'events',
-        element: <EventsRootLayout />,
+    {
+        path: '/',
+        element: <RootLayout/>,
+        errorElement: <ErrorPage/>,
         children: [
-          {
-            index: true,
-            element: <EventsPage />,
-            loader: eventsLoader,
-          },
-          {
-            path: ':eventId',
-            id: 'event-detail',
-            loader: eventDetailLoader,
-            children: [
-              {
-                index: true,
-                element: <EventDetailPage />,
-                action: deleteEventAction,
-              },
-              {
-                path: 'edit',
-                element: <EditEventPage />,
-                action: manipulateEventAction,
-              },
-            ],
-          },
-          {
-            path: 'new',
-            element: <NewEventPage />,
-            action: manipulateEventAction,
-          },
+            {index: true, element: <HomePage/>},
+            {
+                path: 'events',
+                element: <EventsRootLayout/>,
+                children: [
+                    {
+                        index: true,
+                        element: <EventsPage/>,
+                        loader: eventsLoader,
+                    },
+                    {
+                        path: ':eventId',
+                        id: 'event-detail',
+                        loader: eventDetailLoader,
+                        children: [
+                            {
+                                index: true,
+                                element: <EventDetailPage/>,
+                                action: deleteEventAction,
+                            },
+                            {
+                                path: 'edit',
+                                element: <EditEventPage/>,
+                                action: manipulateEventAction,
+                            },
+                        ],
+                    },
+                    {
+                        path: 'new',
+                        element: <NewEventPage/>,
+                        action: manipulateEventAction,
+                    },
+                ],
+            },
         ],
-      },
-      {
-        path: 'newsletter',
-        element: <NewsletterPage />,
-        action: newsletterAction,
-      },
-    ],
-  },
+    },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
