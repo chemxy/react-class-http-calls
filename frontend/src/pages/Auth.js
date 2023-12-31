98cdef4e3f1b8c1d26f932cd6c9987a149386d1d
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2023-12-30 17:45:56 -0800

    add delete event function

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,6 +1,5 @@
 import "../App.css";
 import AuthForm from "../components/AuthForm";
-import { useEffect } from "react";
 
 function AuthPage() {
 
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { json, useParams, } from 'react-router-dom';
-
-import EventItem from '../components/EventItem';
+import { json, NavLink, useNavigate, useParams, } from 'react-router-dom';
+import "../App.css";
 
 function EventDetailPage() {
     const [event, setEvent] = useState(undefined);
     const params = useParams();
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch('http://localhost:8080/events/' + params.eventId)
@@ -28,11 +28,55 @@ function EventDetailPage() {
         )
     }, [])
 
-    return (
-        <>
-            {event && <EventItem event={event}/>}
-        </>
-    );
+
+    function handleDelete() {
+        console.log("delete event id: " + event.id);
+        const url = 'http://localhost:8080/events/' + event.id;
+
+        fetch(url, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then(response => {
+            if (response.status === 422) {
+                return response;
+            }
+
+            if (!response.ok) {
+                throw json({message: 'Could not sign up.'}, {status: 500});
+            }
+
+            return response.json();
+        }).then(() => {
+            console.log("deleted");
+            navigate("/events")
+        })
+    }
+
+    if (event) {
+        return (
+            <>
+                <div className="event">
+                    <NavLink to="/events"> Return to Events</NavLink>
+                </div>
+                <article className="event">
+                    <img src={event.image} alt={event.title}/>
+                    <h1>{event.title}</h1>
+                    <time>{event.date}</time>
+                    <p>{event.description}</p>
+                    <menu className="actions">
+                        {/*<Link to="edit">Edit</Link>*/}
+                        <button onClick={handleDelete}>Delete</button>
+                    </menu>
+                </article>
+            </>
+        );
+    } else {
+        return <></>
+    }
+
+
 }
 
 export default EventDetailPage;
