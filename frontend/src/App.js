fef46ad4cbd38d86808d0874a704eaf1573bf086
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2024-01-09 15:34:58 -0800

    add use effect example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,45 @@
-import './App.css';
-import LoginPage from "./pages/LoginPage";
-
+effect import './App.css';
+import { useEffect, useState } from "react";
+import { json, Link } from "react-router-dom";
 
 function App() {
 
-    return <LoginPage/>;
+    const [events, setEvents] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8080/events')
+            .then(response => {
+                if (!response.ok) {
+                    // console.log("events reponse not ok")
+                    throw json(
+                        {message: 'Could not fetch events.'},
+                        {
+                            status: 500,
+                        }
+                    );
+                } else {
+                    // console.log("events reponse ok: ")
+                    return response.json();
+                }
+            }).then(resData => {
+                setEvents(resData.events)
+            }
+        )
+    }, [])
+    console.log(events)
+
+    return <ul>
+        {events.map((event) => (
+            <li key={event.id}>
+                <img src={event.image} alt={event.title}/>
+                <div>
+                    <h2>{event.title}</h2>
+                    <time>{event.date}</time>
+                </div>
+
+            </li>
+        ))}
+    </ul>;
 }
 
 export default App;
